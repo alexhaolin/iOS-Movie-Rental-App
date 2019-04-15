4b067e9f8d53f2d15469c034b49e3cd39a626434
alex <xu.haol@husky.neu.edu>
2019-04-15 15:07:04 -0400

    Initial Commit

---
+++ Final/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Final
+//
+//  Created by Alex on 4/15/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Final/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Final
+//
+//  Created by Alex on 4/15/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
